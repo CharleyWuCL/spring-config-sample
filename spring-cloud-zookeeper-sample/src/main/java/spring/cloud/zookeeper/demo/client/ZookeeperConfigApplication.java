213e4f31185c65db6aa63d1d56c11700173d7a51
CharleyWuCL <charleywucl@gmail.com>
2018-12-20 18:38:32 +0800

    remove something.

--- spring-cloud-zookeeper-sample/src/main/java/spring/cloud/zookeeper/demo/client/ZookeeperConfigApplication.java
+++ spring-cloud-zookeeper-sample/src/main/java/spring/cloud/zookeeper/demo/client/ZookeeperConfigApplication.java
@@ -1,11 +1,3 @@
-/*
- * Copyright (c) 2018 Oracle. All rights reserved.
- *
- * This material is the confidential property of Oracle Corporation or its
- * licensors and may be used, reproduced, stored or transmitted only in
- * accordance with a valid Oracle license or sublicense agreement.
- */
-
 package spring.cloud.zookeeper.demo.client;
 
 import org.springframework.beans.factory.annotation.Value;
